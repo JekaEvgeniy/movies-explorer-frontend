ca01bb5aa7d463561bdddacaae2e30401832b485
JekaEvgeniy <pruven@yandex.ru>
2023-10-18 22:00:31 +0300

    import Portfolio

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,7 @@ import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
 
 function Main() {
   return (
@@ -15,6 +16,7 @@ function Main() {
         <AboutProject />
         <Techs />
         <AboutMe />
+        <Portfolio />
       </main>
     </>
   );
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Sprite from '../../images/common/sprite.svg';
+
+function Portfolio(){
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__section-header">Портфолио</h2>
+
+      <ul className="portfolio-items">
+        <li className="portfolio-items__item">
+          <a href="https://jekaevgeniy.github.io/russian-travel/index.html" className="portfolio__link" target="_blank">
+            <span className="portfolio__link-title">Статичный сайт</span>
+            <span className="portfolio__link-ico">
+              <svg className="image-contain" xmlns="http://www.w3.org/2000/svg">
+                <use href={`${Sprite}#portfolio-arrow`}></use>
+              </svg>
+            </span>
+          </a>
+        </li>
+        <li className="portfolio-items__item">
+          <a href="https://jekaevgeniy.github.io/russian-travel/index.html" className="portfolio__link" target="_blank">
+            <span className="portfolio__link-title">Адаптивный сайт</span>
+            <span className="portfolio__link-ico">
+              <svg className="image-contain" xmlns="http://www.w3.org/2000/svg">
+                <use href={`${Sprite}#portfolio-arrow`}></use>
+              </svg>
+            </span>
+          </a>
+        </li>
+        <li className="portfolio-items__item">
+          <a href="https://jekaevgeniy.github.io/mesto/index.html" className="portfolio__link" target="_blank">
+            <span className="portfolio__link-title">Одностраничное приложение</span>
+            <span className="portfolio__link-ico">
+              <svg className="image-contain" xmlns="http://www.w3.org/2000/svg">
+                <use href={`${Sprite}#portfolio-arrow`}></use>
+              </svg>
+            </span>
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+export default Portfolio;
