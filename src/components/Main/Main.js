d508c830c446d8bd2060cf1f4be418ca475877fc
JekaEvgeniy <pruven@yandex.ru>
2023-10-18 21:14:14 +0300

    import Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 function Main() {
   return (
@@ -11,6 +12,7 @@ function Main() {
       <main className="content">
         <Promo />
         <AboutProject />
+        <Techs />
       </main>
     </>
   );
