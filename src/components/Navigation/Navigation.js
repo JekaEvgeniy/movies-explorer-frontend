d203050c547bb18058038d0ee9b0338c8579b0a9
JekaEvgeniy <pruven@yandex.ru>
2023-10-21 04:06:07 +0300

    codereview: fix header tag

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <div className="header-actions">
+    <nav className="header-actions">
       <Link className="header-actions__link header-actions__link_type_text" to="/signup">Регистрация</Link>
       <Link className="header-actions__link header-actions__link_type_btn" to="/signin">Войти</Link>
-    </div>
+    </nav>
   )
 }
 
